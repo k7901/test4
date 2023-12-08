0858d93ddbb30fef4e81fa8f2a1fb6f8b1e8ca2e
KokiOhara <144040393+k7901@users.noreply.github.com>
2023-12-08 07:16:08 +0000

    first commit

---
+++ rails6-bookers2-debug-ruby3-main/app/controllers/books_controller.rb
@@ -0,0 +1,63 @@
+class BooksController < ApplicationController
+before_action :is_matching_login_user, only: [:edit, :update]
+
+  def show
+    @book = Book.find(params[:id])
+    @user = current_user
+    @books = @user.books
+
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
+    else
+       @books = Book.all
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+  def is_matching_login_user
+    user = Book.find(params[:id]).user
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+end
